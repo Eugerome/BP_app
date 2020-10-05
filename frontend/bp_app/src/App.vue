853b50304878cff1ae7d0cd91cd23a9a74e901e2
root <root@DESKTOP-TOLQK92.localdomain>
2020-10-05 16:37:41 +0100

    Changed node image; Base setup for frontend

---
+++ frontend/bp_app/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <Records v-bind:records="records"/>
+  </div>
+</template>
+
+<script>
+import Records from "./components/Records"
+
+export default {
+  name: 'App',
+  components: {
+    Records
+  },
+  data () {
+    return {
+      records: [
+        {
+          record_id: 549,
+          timestamp: "2020-10-05T09:23:52Z",
+          bp_upper: 156,
+          bp_lower: 63,
+          notes: "Test"
+        },
+        {
+          record_id: 550,
+          timestamp: "2020-10-04T09:23:52Z",
+          bp_upper: 160,
+          bp_lower: 60,
+          notes: "Test"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ frontend/bp_app/src/components/Records.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Records</h1>
+        <div v-bind:key="record.id" v-for="record in records">
+            <h3>{{ record.notes }}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Records",
+    props: ["records"]
+}
+</script>
+
+<style scoped>
+
+</style>
